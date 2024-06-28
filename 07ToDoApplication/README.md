11fd1c2cc7ef5f568271f7336abbd6008a887aba
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-06-28 09:03:39 +0530

    Todo App Statrted

---
+++ 07ToDoApplication/README.md
@@ -0,0 +1,6 @@
+# React Todo App
+
+### Steps
+
+1. Setup the project and also installed tailwind css.
+2. Create a TodoContext.
---
+++ 07ToDoApplication/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-orange-300 text-center">Todo</h1>
+    </>
+  )
+}
+
+export default App
