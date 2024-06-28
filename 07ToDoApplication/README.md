61f93d4d87a9b764cdfdb9fbd76a437f84889c66
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-06-28 16:17:06 +0530

    Basic Context Functionality Done

--- 07ToDoApplication/README.md
+++ 07ToDoApplication/README.md
@@ -4,3 +4,7 @@
 
 1. Setup the project and also installed tailwind css.
 2. Create a TodoContext.
+3. Create App.jsx UI
+4. Wrap the App.jsx UI with TodoProvider and passed the values.
+5. Add functionality to our functions, provided by the TodoContext
+6. 
--- 07ToDoApplication/src/App.jsx
+++ 07ToDoApplication/src/App.jsx
@@ -1,11 +1,44 @@
+import { TodoProvider } from "./context"
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [...prevTodos, {id: Date.now(), ...todo}]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((oldTodo) => (oldTodo.id === id? todo : oldTodo)),
+    );
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id!== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id? {...todo, completed:!todo.completed} : todo))
+    );
+  }
+
   return (
-    <>
-      <h1 className="bg-orange-300 text-center">Todo</h1>
-    </>
-  )
+    <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
